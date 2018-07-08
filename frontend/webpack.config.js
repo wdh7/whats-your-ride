d54deb386021198ba8f00b43ee6e00e7c9001212
William Huynh <31399549+wdh7@users.noreply.github.com>
2018-07-08 15:35:32 -0700

    Initialize front end

---
+++ frontend/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: ['babel-polyfill', 'whatwg-fetch', './app/index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {test: /\.(js)$/, use: 'babel-loader'},
+      {test: /\.css$/, use: ['style-loader', 'css-loader']}
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './app/index.html'
+    })
+  ],
+  mode: 'development',
+  devServer: {
+    historyApiFallback: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        secure: false
+      }
+    }
+  }
+}
