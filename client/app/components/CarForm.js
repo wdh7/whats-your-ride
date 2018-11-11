4d2164a8ba0a02853616552a10d9fdfa495022fd
William Huynh <31399549+wdh7@users.noreply.github.com>
2018-11-10 18:48:35 -0800

    update bootstrap and car form

--- client/app/components/CarForm.js
+++ client/app/components/CarForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Button, Form, FormGroup, FormText, Label, Input } from 'reactstrap';
 
 function CarForm({ handleInput }) {
   return (
@@ -8,18 +8,22 @@ function CarForm({ handleInput }) {
       <FormGroup>
         <Label for="make">Make</Label>
         <Input type="text" name="make" id="make" onChange={handleInput} />
+        <FormText>Field is required</FormText>
       </FormGroup>
       <FormGroup>
         <Label for="model">Model</Label>
         <Input type="text" name="model" id="model" onChange={handleInput} />
+        <FormText>Field is required</FormText>
       </FormGroup>
       <FormGroup>
         <Label for="year">Year</Label>
         <Input type="text" name="year" id="year" onChange={handleInput} />
+        <FormText>Field is required</FormText>
       </FormGroup>
       <FormGroup>
         <Label for="description">Description</Label>
         <Input type="text" name="description" id="description" onChange={handleInput} />
+        <FormText>Field is required</FormText>
       </FormGroup>
       <FormGroup>
         <Label for="img">Image</Label>
