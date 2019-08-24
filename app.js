d680d68efaa7d38c1011fb994fa73c1941b3f6f1
William Huynh <31399549+wdh7@users.noreply.github.com>
2019-08-24 13:54:17 -0700

    update npm packages

--- app.js
+++ app.js
@@ -17,14 +17,14 @@ const connectionUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/what
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/dist')));
 
-mongoose.connect(connectionUri);
+mongoose.connect(connectionUri, { useNewUrlParser: true });
 mongoose.connection
   .once('open', () => {
     console.log('Connected to MongoDB');
   })
   .once('error', () => {
     console.log('Error connecting to MongoDB');
-  })
+  });
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -36,7 +36,7 @@ app.use('/api/users', usersRoutes);
 app.use('/api/cars', carsRoutes);
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/dist/index.html'));
+  res.sendFile(path.join(__dirname + '/client/dist/index.html'));
 });
 
 // Validation Error handling middleware
@@ -45,16 +45,15 @@ app.use((err, req, res, next) => {
   const errorKeys = Object.keys(errors);
   const newErrors = { errors: {} };
 
-  errorKeys.forEach(key => {
+  errorKeys.forEach((key) => {
     newErrors['errors'][key] = errors[key].message;
-  })
+  });
 
   // log the errors to console
   // and send back error response to client
   console.log(newErrors);
   // code 422: can't process
   res.status(422).json(newErrors);
-})
-
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
